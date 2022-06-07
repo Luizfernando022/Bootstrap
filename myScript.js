cc77941ebe04c5dcace3dcc1fc4686a686331b3e
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-06-07 12:08:45 -0300

    .

--- myScript.js
+++ myScript.js
@@ -1,15 +1,26 @@
 let items = document.querySelectorAll("[data-animation]")
 
 
+let lockmode = 0
 
-window.addEventListener('scroll',aos)
+
+
+window.addEventListener('scroll',function(){
+    if(lockmode == 0){
+        lockmode = 1
+        aos()
+        setTimeout(()=>{
+            lockmode = 0
+        },150)
+    }
+})
 
 
 aos()
 
 
 function aos(){
-    
+    console.log('a')
     let windowTop = scrollY + innerHeight * 0.75
 
 
@@ -25,5 +36,23 @@ function aos(){
 
 
     })
+
+}
+
+textAnimation()
+
+function textAnimation(){
+    let i = 0
+    let timer = 1000
+    let array = ['S','e','j','a']
+
+    while(i < array.length){
+        setTimeout(()=>{
+            console.log(array[i])
+        },timer)
+        i++
+        timer+=1000
+    }
+        
 
 }
